768f5bf069fc35cffd3a49a467f41c5632d78f69
Shainky Jain <jainshainky123@gmail.com>
2024-06-20 21:34:29 +0530

    -gh pages

--- src/component/Loading.jsx
+++ src/component/Loading.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Loading = () => {
   return (
-    <div className='w-full h-full flex justify-center items-center '> 
-
-<h1 className='text-6xl'>Loading...</h1>
-
+    <div className="w-full h-full flex justify-center items-center ">
+      <h1 className="text-6xl">Loading...</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Loading
+export default Loading;
